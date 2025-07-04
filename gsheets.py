5d7c9c458618895f3995dd7dc8b6e268a1195989
MadmaxRU <83331181+MadmaxRU@users.noreply.github.com>
2025-07-04 10:56:02 +0300

    Update gsheets.py

--- gsheets.py
+++ gsheets.py
@@ -1,25 +1,21 @@
+import gspread
 import os
 import json
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
+from google.oauth2.service_account import Credentials
 
 def write_to_gsheet(data):
-    scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
     creds_dict = json.loads(os.getenv("GOOGLE_CREDS_JSON"))
-    creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
-    client = gspread.authorize(creds)
-
-    spreadsheet = client.open_by_key(os.getenv("SPREADSHEET_ID"))
-    worksheet = spreadsheet.sheet1
-
-    worksheet.append_row([
+    creds = Credentials.from_service_account_info(creds_dict)
+    gc = gspread.authorize(creds)
+    sheet = gc.open("CPAauto_leads").sheet1
+    row = [
         data.get("lead_type", ""),
+        data.get("name", ""),
         data.get("city", ""),
         data.get("car_brand", ""),
-        data.get("year", ""),
         data.get("payment_method", ""),
-        data.get("name", ""),
-        data.get("phone", ""),
         data.get("from_abroad", ""),
-        data.get("agreement", "")
-    ])
+        data.get("agreement", ""),
+        data.get("phone", "")
+    ]
+    sheet.append_row(row)
