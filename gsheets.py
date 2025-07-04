93a4340096c0b3c7ac3784211cd0c4901e4338be
MadmaxRU <83331181+MadmaxRU@users.noreply.github.com>
2025-07-04 10:43:05 +0300

    Update gsheets.py

--- gsheets.py
+++ gsheets.py
@@ -1,26 +1,22 @@
+import os
+import json
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
-import json
-import os
-
-# Подключение к Google Sheets через credentials.json
-scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-
-with open("/etc/secrets/credentials.json") as f:
-    creds_dict = json.load(f)
-
-creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
-client = gspread.authorize(creds)
-
-# Получаем таблицу по SHEET_ID из переменной окружения
-sheet = client.open_by_key(os.getenv("SHEET_ID")).sheet1
 
 def write_to_gsheet(data):
-    print("🚀 Передача данных в таблицу:", data)
-    sheet.append_row([
+    scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+    creds_dict = json.loads(os.getenv("GOOGLE_CREDS_JSON"))
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
+    client = gspread.authorize(creds)
+
+    spreadsheet = client.open_by_key(os.getenv("SPREADSHEET_ID"))
+    worksheet = spreadsheet.sheet1
+
+    worksheet.append_row([
         data.get("lead_type", ""),
         data.get("city", ""),
         data.get("car_brand", ""),
+        data.get("year", ""),
         data.get("payment_method", ""),
         data.get("name", ""),
         data.get("phone", ""),
