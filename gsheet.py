cc27232fb3e096a701d732313eda3684894da96e
MadmaxRU <83331181+MadmaxRU@users.noreply.github.com>
2025-07-11 16:31:25 +0300

    Update gsheet.py

--- gsheet.py
+++ gsheet.py
@@ -6,7 +6,7 @@ def write_to_gsheet(data):
     scopes = ['https://www.googleapis.com/auth/spreadsheets']
     credentials = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scopes)
     client = gspread.authorize(credentials)
-    sheet = client.open("CPAauto заявки").sheet1
+    sheet = client.open("CPAauto").worksheet("Лист1")
     sheet.append_row([
         data.get("deal_type", ""),
         data.get("car_brand", ""),
